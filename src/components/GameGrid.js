221e62781ee9222a27ba1c9b36444f1c3e53b01d
JinLisek <tastyramen@gmail.com>
2020-10-11 18:45:16 +0200

    Cells correctly behave like in game of life, tick is 1 second hardcode

--- src/components/GameGrid.js
+++ src/components/GameGrid.js
@@ -5,29 +5,79 @@ import ResponsiveEmbed from "react-bootstrap/ResponsiveEmbed";
 import Container from "react-bootstrap/Container";
 import VisualTile from "./VisualTile";
 
-function GameGrid(props) {
-  return (
-    <Container style={{ width: 600, height: "auto" }}>
-      <ResponsiveEmbed>
-        <Table bordered size="sm">
-          <tbody>
-            {props.grid.map((row) => (
-              <tr key={"row: " + row[0].y}>
-                {row.map((tile) => (
-                  <VisualTile tile={tile} key={"x: " + tile.x + ", y: " + tile.y} />
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </ResponsiveEmbed>
-    </Container>
-  );
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
-const mapState = (state) => {
-  const { grid } = state;
+function isNeighbourAlive(x, y, grid) {
+  if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length) return false;
+  return grid[x][y].isFilled;
+}
+
+class GameGrid extends React.Component {
+  componentDidMount = () => {
+    this.timerId = setInterval(() => {
+      const maxSize = this.props.grid.length;
+      const randomX = getRandomInt(maxSize);
+      const randomY = getRandomInt(maxSize);
+      const oldGrid = this.props.grid;
+      let updatedGrid = [...oldGrid];
+
+      for (let y = 0; y < updatedGrid.length; ++y) {
+        for (let x = 0; x < updatedGrid[y].length; ++x) {
+          let aliveNeighbours = 0;
+          if (isNeighbourAlive(x - 1, y - 1, oldGrid)) aliveNeighbours++;
+          if (isNeighbourAlive(x, y - 1, oldGrid)) aliveNeighbours++;
+          if (isNeighbourAlive(x + 1, y - 1, oldGrid)) aliveNeighbours++;
+          if (isNeighbourAlive(x - 1, y, oldGrid)) aliveNeighbours++;
+          if (isNeighbourAlive(x + 1, y, oldGrid)) aliveNeighbours++;
+          if (isNeighbourAlive(x - 1, y + 1, oldGrid)) aliveNeighbours++;
+          if (isNeighbourAlive(x, y + 1, oldGrid)) aliveNeighbours++;
+          if (isNeighbourAlive(x + 1, y + 1, oldGrid)) aliveNeighbours++;
+
+          let cell = updatedGrid[x][y];
+          if (!cell.isFilled && aliveNeighbours == 3) cell.isFilled = true;
+          else if (cell.isFilled && (aliveNeighbours < 2 || aliveNeighbours > 3)) cell.isFilled = false;
+        }
+      }
+
+      this.props.updateGrid(updatedGrid);
+    }, 1000);
+  };
+
+  componentWillUnmount = () => {
+    clearInterval(this.timerId);
+  };
+
+  render = () => {
+    return (
+      <Container style={{ width: 600, height: "auto" }}>
+        <ResponsiveEmbed>
+          <Table bordered size="sm">
+            <tbody>
+              {this.props.grid.map((row) => (
+                <tr key={"row: " + row[0].y}>
+                  {row.map((tile) => (
+                    <VisualTile tile={tile} key={"x: " + tile.x + ", y: " + tile.y} />
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </ResponsiveEmbed>
+      </Container>
+    );
+  };
+}
+
+const mapStateToProps = ({ grid }) => {
   return { grid };
 };
 
-export default connect(mapState)(GameGrid);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateGrid: (updatedGrid) => dispatch({ type: "grid/updateGrid", updatedGrid: updatedGrid }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameGrid);
